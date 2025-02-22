7b9f7d20ed29450adbbe372dd7b2ddcaa4894d1c
Harry-Yates <hyates1@gmail.com>
2025-02-22 21:29:40 +0100

    removed unused icons

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,7 +2,6 @@
 
 import Link from 'next/link'
 import { useState, useEffect } from 'react'
-import { XMarkIcon, Bars3Icon } from '@heroicons/react/24/outline'
 import AnimatedMenuButton from './AnimatedMenuButton'
 
 export default function Navbar() {
